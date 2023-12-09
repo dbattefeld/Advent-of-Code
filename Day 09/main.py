6b516a39daa122ee84400c79f2d105a931b77867
Dominik Battefeld <dbattefeld@techfak.de>
2023-12-09 21:02:05 +0100

    Update day 09

--- Day 09/main.py
+++ Day 09/main.py
@@ -4,11 +4,11 @@ with open("input.txt") as file:
 p1 = 0
 p2 = 0
 for sequence in sequences:
-    depth = 0
+    sign = 1
     while any(sequence):
         p1 += sequence[-1]
-        p2 += (-1) ** depth * sequence[0]
+        p2 += sign * sequence[0]
         sequence = [q - p for p, q in zip(sequence[:-1], sequence[1:])]
-        depth += 1
+        sign *= (-1)
 print("Last values:", p1)
 print("First values:", p2)
